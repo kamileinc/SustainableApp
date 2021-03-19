e77bb39416ef89fe09e4d77b03099c26e9c52778
Karolis Inciura <kinciura@gmail.com>
2021-03-19 18:52:22 +0200

    2021-03-19 broadcast ir notifications pradzia

--- app/src/main/java/com/example/sustainableapp/views/AllResultsFragment.java
+++ app/src/main/java/com/example/sustainableapp/views/AllResultsFragment.java
@@ -1,7 +1,14 @@
 package com.example.sustainableapp.views;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -11,8 +18,14 @@ import android.view.ViewGroup;
 
 import com.example.sustainableapp.R;
 
+import java.util.Calendar;
+
+import static android.content.Context.ALARM_SERVICE;
+
 public class AllResultsFragment extends Fragment {
     String userID;
+
+
     public AllResultsFragment() {
         // Required empty public constructor
     }
@@ -38,6 +51,50 @@ public class AllResultsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_all_results, container, false);
+        View view = inflater.inflate(R.layout.fragment_all_results, container, false);
+        IntentFilter filtras = new IntentFilter();
+        filtras.addAction("2_Programos_nesist_isreikst_trans");
+        view.getContext().registerReceiver(new transl_imtuvas2(), filtras);
+        createBroadcast(view);
+        Log.i("mano", "broadcast created?");
+        return view;
+    }
+    public void createBroadcast(View view) {
+        Calendar alarmFor = Calendar.getInstance();
+        alarmFor.set(Calendar.HOUR_OF_DAY, 16);
+        alarmFor.set(Calendar.MINUTE, 22);
+        alarmFor.set(Calendar.SECOND, 0);
+
+        Intent MyIntent = new Intent(view.getContext(), transl_imtuvas2.class);
+        PendingIntent MyPendIntent = PendingIntent.getBroadcast(view.getContext(), 0, MyIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        AlarmManager MyAlarm = (AlarmManager) view.getContext().getSystemService(Context.ALARM_SERVICE);
+        MyAlarm.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, alarmFor.getTimeInMillis(), MyPendIntent);
+        view.getContext().sendBroadcast(MyIntent);
+        Log.i("mano", "broadcast created???");
+    }
+    public static class transl_imtuvas2 extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent i) {
+            /*
+            Log.i("mano", "1 programa: transl_imtuvas2");
+            String intent = i.getAction();
+            Bundle rezExtra = i.getExtras();
+            siuntejas = rezExtra.getString("siuntejas");
+            String papildoma_info = rezExtra.getString("papildoma_info");
+            {
+                final String pranesimas =
+                        "Gautas naujas tarnsliavimo pranešimas" +
+                                "\n Intentas: " + intent +
+                                "\n Siuntėjas: " + siuntejas +
+                                " \n Papildoma intento informacija: \n" + papildoma_info;
+                textView.setText(pranesimas);
+                //Toast.makeText(MainActivity.this, pranesimas, Toast.LENGTH_SHORT).show();
+                sendNotification();
+            }
+            */
+            Log.i("mano", "onReceive() called");
+        }
     }
 }
--- app/src/main/java/com/example/sustainableapp/views/UserActivity.java
+++ app/src/main/java/com/example/sustainableapp/views/UserActivity.java
@@ -1,20 +1,35 @@
 package com.example.sustainableapp.views;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.sustainableapp.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class UserActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigation;
@@ -23,12 +38,18 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        createNotificationChannel();
         bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
         openFragment(WheelFragment.newInstance("", ""));
         Intent intent = getIntent();
         userID = intent.getStringExtra("userID");
         Log.i("mano", "activity: " + userID);
+        IntentFilter filtras = new IntentFilter();
+        filtras.addAction("broadcastas");
+        this.registerReceiver(new transl_imtuvas2(), filtras);
+        createBroadcast();
+        Log.i("mano", "broadcast created?activityje");
         openFragment(new WheelFragment());
         //openFragment(new EditProfileFragment());
     }
@@ -89,5 +110,60 @@ public class UserActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
         startActivity(intent);
         finish();
+    }
+    public void createBroadcast() {
+        Calendar alarmFor = Calendar.getInstance();
+
+        Calendar alarmFor2 = Calendar.getInstance();
+        alarmFor.set(Calendar.HOUR_OF_DAY, 18);
+        alarmFor.set(Calendar.MINUTE, 51);
+        alarmFor.set(Calendar.SECOND, 0);
+
+        Intent MyIntent = new Intent(this, UserActivity.transl_imtuvas2.class);
+        PendingIntent MyPendIntent = PendingIntent.getBroadcast(this, 0, MyIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        AlarmManager MyAlarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        MyAlarm.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, alarmFor.getTimeInMillis(), MyPendIntent);
+
+        Log.i("mano", "broadcast created???activityje: " + alarmFor.getTime());
+        Log.i("mano", "broadcast created???activityje: " + alarmFor2.getTime());
+    }
+    public static class transl_imtuvas2 extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent i) {
+            Log.i("mano", "broadcast onReceive() called");
+            sendNotification(context);
+        }
+    }
+    private void createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "textTitle";
+            String description ="textContent";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("1", name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+    public static void sendNotification(Context context) {
+        Intent resultIntent = new Intent(context, UserActivity.class);
+        resultIntent.putExtra("tekstas", "tekstas?");
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "1")
+                .setSmallIcon(R.drawable.ic_baseline_category_24)
+                .setContentTitle("Pranešimas")
+                .setContentText("Užpildyk šios dienos užduotis")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(resultPendingIntent);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(1, builder.build());
     }
 }
