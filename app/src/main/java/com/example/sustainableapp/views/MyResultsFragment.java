25eeaf63c7670f0cc9b9ca64dbd90c2b113c37d6
Karolis Inciura <kinciura@gmail.com>
2021-03-21 15:43:16 +0200

    2021-03-21 myresults food action patobulinta

--- app/src/main/java/com/example/sustainableapp/views/MyResultsFragment.java
+++ app/src/main/java/com/example/sustainableapp/views/MyResultsFragment.java
@@ -102,26 +102,15 @@ public class MyResultsFragment extends Fragment {
             public void onChange() {
                 Log.i("mano", "cia");
                 if (FAData != null) {
-                    double fa1 = 0;
-                    double fa2 = 0;
-                    double fa3 = 0;
-                    double fa4 = 0;
-                    double fa5 = 0;
-                    double fa6 = 0;
-                    double fa7 = 0;
+                    ArrayList<Double> arr = new ArrayList<Double>();
                     int numberOfActivity = 0;
-                    boolean boo1 = false;
-                    boolean boo2 = false;
-                    boolean boo3 = false;
-                    boolean boo4 = false;
-                    boolean boo5 = false;
-                    boolean boo6 = false;
-                    boolean boo7 = false;
                     Date today = new Date(System.currentTimeMillis());
                     SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd");
                     String todayStr = formatter.format(today);
                     Date dateToCheck = new Date();
+
                     for (int i = 0; i<FAData.size();i++) {
+                        double temp = 0;
                         SustainableActionController sac = new SustainableActionController();
                         try {
                             dateToCheck = formatter.parse(FAData.get(i).getDate());
@@ -130,263 +119,47 @@ public class MyResultsFragment extends Fragment {
                         }
                         if (sac.isDateInDates(dateToCheck, FAData.get(0).getDate(), todayStr)) {
                             numberOfActivity++;
-                            if (i == 0) {
-                                boo1 = true;
-                            } else if (i == 1) {
-                                boo2 = true;
-                            } else if (i == 2) {
-                                boo3 = true;
-                            } else if (i == 3) {
-                                boo4 = true;
-                            } else if (i == 4) {
-                                boo5 = true;
-                            } else if (i == 5) {
-                                boo6 = true;
-                            } else if (i == 6) {
-                                boo7 = true;
-                            }
                             if (FAData.get(i).getBreakfastFood().equals("0")) {
-                                if (i == 0) {
-                                    fa1 = fa1 + 10;
-                                } else if (i == 1) {
-                                    fa2 = fa2 + 10;
-                                } else if (i == 2) {
-                                    fa3 = fa3 + 10;
-                                } else if (i == 3) {
-                                    fa4 = fa4 + 10;
-                                } else if (i == 4) {
-                                    fa5 = fa5 + 10;
-                                } else if (i == 5) {
-                                    fa6 = fa6 + 10;
-                                } else if (i == 6) {
-                                    fa7 = fa7 + 10;
-                                }
+                                temp = temp + 10;
                             } else if (FAData.get(i).getBreakfastFood().equals("1")) {
-                                if (i == 0) {
-                                    fa1 = fa1 + 7.5;
-                                } else if (i == 1) {
-                                    fa2 = fa2 + 7.5;
-                                } else if (i == 2) {
-                                    fa3 = fa3 + 7.5;
-                                } else if (i == 3) {
-                                    fa4 = fa4 + 7.5;
-                                } else if (i == 4) {
-                                    fa5 = fa5 + 7.5;
-                                } else if (i == 5) {
-                                    fa6 = fa6 + 7.5;
-                                } else if (i == 6) {
-                                    fa7 = fa7 + 7.5;
-                                }
+                                temp = temp + 7.5;
                             } else if (FAData.get(i).getBreakfastFood().equals("2")) {
-                                if (i == 0) {
-                                    fa1 = fa1 + 5;
-                                } else if (i == 1) {
-                                    fa2 = fa2 + 5;
-                                } else if (i == 2) {
-                                    fa3 = fa3 + 5;
-                                } else if (i == 3) {
-                                    fa4 = fa4 + 5;
-                                } else if (i == 4) {
-                                    fa5 = fa5 + 5;
-                                } else if (i == 5) {
-                                    fa6 = fa6 + 5;
-                                } else if (i == 6) {
-                                    fa7 = fa7 + 5;
-                                }
+                                temp = temp + 5;
                             } else if (FAData.get(i).getBreakfastFood().equals("3")) {
-                                if (i == 0) {
-                                    fa1 = fa1 + 2.5;
-                                } else if (i == 1) {
-                                    fa2 = fa2 + 2.5;
-                                } else if (i == 2) {
-                                    fa3 = fa3 + 2.5;
-                                } else if (i == 3) {
-                                    fa4 = fa4 + 2.5;
-                                } else if (i == 4) {
-                                    fa5 = fa5 + 2.5;
-                                } else if (i == 5) {
-                                    fa6 = fa6 + 2.5;
-                                } else if (i == 6) {
-                                    fa7 = fa7 + 2.5;
-                                }
+                                temp = temp + 2.5;
                             }
 
                             if (FAData.get(i).getLunchFood().equals("0")) {
-                                if (i == 0) {
-                                    fa1 = fa1 + 10;
-                                } else if (i == 1) {
-                                    fa2 = fa2 + 10;
-                                } else if (i == 2) {
-                                    fa3 = fa3 + 10;
-                                } else if (i == 3) {
-                                    fa4 = fa4 + 10;
-                                } else if (i == 4) {
-                                    fa5 = fa5 + 10;
-                                } else if (i == 5) {
-                                    fa6 = fa6 + 10;
-                                } else if (i == 6) {
-                                    fa7 = fa7 + 10;
-                                }
+                                temp = temp + 10;
                             } else if (FAData.get(i).getLunchFood().equals("1")) {
-                                if (i == 0) {
-                                    fa1 = fa1 + 7.5;
-                                } else if (i == 1) {
-                                    fa2 = fa2 + 7.5;
-                                } else if (i == 2) {
-                                    fa3 = fa3 + 7.5;
-                                } else if (i == 3) {
-                                    fa4 = fa4 + 7.5;
-                                } else if (i == 4) {
-                                    fa5 = fa5 + 7.5;
-                                } else if (i == 5) {
-                                    fa6 = fa6 + 7.5;
-                                } else if (i == 6) {
-                                    fa7 = fa7 + 7.5;
-                                }
+                                temp = temp + 7.5;
                             } else if (FAData.get(i).getLunchFood().equals("2")) {
-                                if (i == 0) {
-                                    fa1 = fa1 + 5;
-                                } else if (i == 1) {
-                                    fa2 = fa2 + 5;
-                                } else if (i == 2) {
-                                    fa3 = fa3 + 5;
-                                } else if (i == 3) {
-                                    fa4 = fa4 + 5;
-                                } else if (i == 4) {
-                                    fa5 = fa5 + 5;
-                                } else if (i == 5) {
-                                    fa6 = fa6 + 5;
-                                } else if (i == 6) {
-                                    fa7 = fa7 + 5;
-                                }
+                                temp = temp + 5;
                             } else if (FAData.get(i).getLunchFood().equals("3")) {
-                                if (i == 0) {
-                                    fa1 = fa1 + 2.5;
-                                } else if (i == 1) {
-                                    fa2 = fa2 + 2.5;
-                                } else if (i == 2) {
-                                    fa3 = fa3 + 2.5;
-                                } else if (i == 3) {
-                                    fa4 = fa4 + 2.5;
-                                } else if (i == 4) {
-                                    fa5 = fa5 + 2.5;
-                                } else if (i == 5) {
-                                    fa6 = fa6 + 2.5;
-                                } else if (i == 6) {
-                                    fa7 = fa7 + 2.5;
-                                }
+                                temp = temp + 2.5;
                             }
 
                             if (FAData.get(i).getDinnerFood().equals("0")) {
-                                if (i == 0) {
-                                    fa1 = fa1 + 10;
-                                } else if (i == 1) {
-                                    fa2 = fa2 + 10;
-                                } else if (i == 2) {
-                                    fa3 = fa3 + 10;
-                                } else if (i == 3) {
-                                    fa4 = fa4 + 10;
-                                } else if (i == 4) {
-                                    fa5 = fa5 + 10;
-                                } else if (i == 5) {
-                                    fa6 = fa6 + 10;
-                                } else if (i == 6) {
-                                    fa7 = fa7 + 10;
-                                }
+                                temp = temp + 10;
                             } else if (FAData.get(i).getDinnerFood().equals("1")) {
-                                if (i == 0) {
-                                    fa1 = fa1 + 7.5;
-                                } else if (i == 1) {
-                                    fa2 = fa2 + 7.5;
-                                } else if (i == 2) {
-                                    fa3 = fa3 + 7.5;
-                                } else if (i == 3) {
-                                    fa4 = fa4 + 7.5;
-                                } else if (i == 4) {
-                                    fa5 = fa5 + 7.5;
-                                } else if (i == 5) {
-                                    fa6 = fa6 + 7.5;
-                                } else if (i == 6) {
-                                    fa7 = fa7 + 7.5;
-                                }
+                                temp = temp + 7.5;
                             } else if (FAData.get(i).getDinnerFood().equals("2")) {
-                                if (i == 0) {
-                                    fa1 = fa1 + 5;
-                                } else if (i == 1) {
-                                    fa2 = fa2 + 5;
-                                } else if (i == 2) {
-                                    fa3 = fa3 + 5;
-                                } else if (i == 3) {
-                                    fa4 = fa4 + 5;
-                                } else if (i == 4) {
-                                    fa5 = fa5 + 5;
-                                } else if (i == 5) {
-                                    fa6 = fa6 + 5;
-                                } else if (i == 6) {
-                                    fa7 = fa7 + 5;
-                                }
+                                temp = temp + 5;
                             } else if (FAData.get(i).getDinnerFood().equals("3")) {
-                                if (i == 0) {
-                                    fa1 = fa1 + 2.5;
-                                } else if (i == 1) {
-                                    fa2 = fa2 + 2.5;
-                                } else if (i == 2) {
-                                    fa3 = fa3 + 2.5;
-                                } else if (i == 3) {
-                                    fa4 = fa4 + 2.5;
-                                } else if (i == 4) {
-                                    fa5 = fa5 + 2.5;
-                                } else if (i == 5) {
-                                    fa6 = fa6 + 2.5;
-                                } else if (i == 6) {
-                                    fa7 = fa7 + 2.5;
-                                }
+                                temp = temp + 2.5;
                             }
-                            fa1 = fa1 / 3;
-                            fa2 = fa2 / 3;
-                            fa3 = fa3 / 3;
-                            fa4 = fa4 / 3;
-                            fa5 = fa5 / 3;
-                            fa6 = fa6 / 3;
-                            fa7 = fa7 / 3;
+                            temp = temp/3;
+                            arr.add(temp);
                         }
-
                     }
-                        drawFAgraph(view, fa1, fa2, fa3, fa4, fa5, fa6, fa7, numberOfActivity, boo1,boo2,boo3,boo4,boo5,boo6,boo7);
-
+                        drawFAgraph(view, arr, numberOfActivity);
                 }
             }});
-        /*
-        final GraphView graph = (GraphView) view.findViewById(R.id.graph);
-        Button button = view.findViewById(R.id.addButton);
-        graph.setVisibility(View.VISIBLE);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    LineGraphSeries < DataPoint > series = new LineGraphSeries < > (new DataPoint[] {
-                            new DataPoint(0, 1),
-                            new DataPoint(1, 1),
-                            new DataPoint(2, 2),
-                            new DataPoint(3, 3),
-                            new DataPoint(4, 4)
-                    });
-                    graph.addSeries(series);
-                } catch (IllegalArgumentException e) {
-                    Toast.makeText(view.getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-                }
-            }
-        });
 
-         */
         return view;
     }
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-
         graph = (GraphView) view.findViewById(R.id.graph);
-
-
-
     }
     public void getUsersSustainableActions() {
         SustainableActionController sac = new SustainableActionController();
@@ -424,7 +197,7 @@ public class MyResultsFragment extends Fragment {
             checkFANotFound(list);
         }
     }
-    public void drawFAgraph(View view, double fa1, double fa2, double fa3, double fa4, double fa5, double fa6, double fa7, int numberOfActivity, boolean boo1, boolean boo2, boolean boo3, boolean boo4, boolean boo5, boolean boo6, boolean boo7) {
+    public void drawFAgraph(View view, ArrayList<Double> arr, int numberOfActivity) {
         graph.setVisibility(View.VISIBLE);
         Log.i("mano", "onviewcreated graph: " + graph);
         try {
@@ -441,27 +214,10 @@ public class MyResultsFragment extends Fragment {
 
              */
             DataPoint[] dp = new DataPoint[numberOfActivity];
-            if (boo1) {
-                dp[0] = new DataPoint(1, fa1);
+            for (int i=0;i<numberOfActivity;i++) {
+                dp[i] = new DataPoint((i+1), arr.get(i));
             }
-            if (boo2) {
-                dp[1] = new DataPoint(2, fa2);
-            }
-            if (boo3) {
-                dp[2] = new DataPoint(3, fa3);
-            }
-            if (boo4) {
-                dp[3] = new DataPoint(4, fa4);
-            }
-            if (boo5) {
-                dp[4] = new DataPoint(5, fa5);
-            }
-            if (boo6) {
-                dp[5] = new DataPoint(6, fa6);
-            }
-            if (boo7) {
-                dp[6] = new DataPoint(7, fa7);
-            }
+
             LineGraphSeries<DataPoint> series = new LineGraphSeries < > (dp);
             series.setDrawDataPoints(true);
             series.setColor(Color.BLACK);
